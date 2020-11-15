70c699efaf8bdded7f418a38adadf83262fff1b2
ahl88 <ahl88@cornell.edu>
2020-11-15 01:04:29 -0500

    added documentation

--- src/board.mli
+++ src/board.mli
@@ -13,10 +13,14 @@ type t
     Requires: [j] is a valid JSON board representation. *)
 val from_json : Yojson.Basic.t -> t
 
+(** [get_type board position] is the type for the tile at [position]. *)
 val get_type : t -> int -> string
 
+(** [get_name board position] is the name for the tile at [position]. *)
 val get_name : t -> int -> string
 
+(** [get_color board position] is the color for the tile at [position]. *)
 val get_color : t -> int -> Property.color option
 
+(** [get_prices board position] is the prices list for the tile at [position].*)
 val get_prices : t -> int -> (string * int) list option
