0384139db468ed595047ad35f760a3cfea320d99
ahl88 <ahl88@cornell.edu>
2020-12-14 00:09:48 -0500

    added more functions for state

--- src/state.ml
+++ src/state.ml
@@ -2,12 +2,10 @@ type pos = int
 
 type state = {
   players : (Player.t * pos) list;
-
   board : Newboard.t;
-
   chance_stack : Cc_card.t list;
-
   community_stack : Cc_card.t list;
+  num_rolls : int;
 }
 
 let create_card (category : Cc_card.category) (board : Newboard.t)= 
@@ -34,12 +32,104 @@ let init_state (players : Player.t list) (board : Newboard.t) = {
   board = board;
   chance_stack = create_card_stack Cc_card.Chance board [] 16;
   community_stack = create_card_stack Cc_card.Community board [] 16;
+  num_rolls = 0;
 }
 
-let current_pos = failwith ""
+let rec pos_helper name players = 
+  match players with
+  | [] -> failwith "Could not find player."
+  | h::t -> if Player.get_name (fst h) = name then snd h 
+    else pos_helper name t
 
-let winner = failwith ""
+let current_pos name state = pos_helper name state.players
 
-let handle_command = failwith ""
+let rec count_balances count players = 
+  match players with
+  | [] -> count
+  | h::t -> if Player.get_money (fst h) > 0 then count_balances (count + 1) t
+    else count_balances count t
 
-let valid_command = failwith ""
+let rec find_winner players = 
+  match players with
+  | [] -> None
+  | h::t -> if Player.get_money (fst h) > 0 then Some (fst h)
+    else find_winner t
+
+let winner state = 
+  if count_balances 0 state.players = 1 then find_winner state.players
+  else None
+
+let player_stat state = Player.player_to_string (fst (List.hd state.players))
+
+let current_tile state board = 
+  let player_pos = snd (List.hd state.players) in 
+  "Currently you are on: " ^ Property.get_name (List.nth board player_pos) ^ "." 
+
+let view_options state = failwith ""
+
+let valid_command (command : Command.t) state = 
+  match command with 
+  | End_Turn ->  true
+  | Forfeit -> failwith ""
+  | Roll -> failwith ""
+  | Buy s -> failwith ""
+  | Sell s -> failwith ""
+  | Collect (s1, s2, s3) -> failwith ""
+  | _ -> failwith "Command should not have been run/DNE."
+
+let move_card_to_bottom lst = (List.tl lst)@[List.hd lst]
+
+let card_helper card state = 
+  match Cc_card.get_action card with
+  | Pay x -> 
+    let player = fst (List.hd state.players) in 
+    let balance = Player.get_money player in 
+    let pay = Player.set_money player (balance + x) in
+    {
+      players = state.players;
+      board = state.board;
+      chance_stack = state.chance_stack;
+      community_stack = state.community_stack;
+      num_rolls = 0;
+    }
+  | Receive x -> failwith ""
+  | OutJail -> failwith ""
+  | GoJail -> failwith ""
+  | Advance s -> failwith ""
+
+let handle_command (command : Command.t) state = 
+  match command with 
+  | End_Turn -> (* Outputs state for next player. *)
+    let curr_player = List.hd state.players in 
+    let new_lst = (List.tl state.players)@[curr_player] in 
+    {
+      players = new_lst;
+      board = state.board;
+      chance_stack = state.chance_stack;
+      community_stack = state.community_stack;
+      num_rolls = 0;
+    }
+  | Forfeit -> (* Removes player from list, and their buildings from board.*)
+    {
+      players = List.tl state.players;
+      board = Newboard.reset_properties (state.board) 
+          (fst (List.hd state.players));
+      chance_stack = state.chance_stack;
+      community_stack = state.community_stack;
+      num_rolls = 0;
+    }
+  (* Updates player position. Automatically draws card if it lands on a chance
+     or community tile. Automatically moves to jail if it lands on go to jail.*)
+  | Roll -> 
+    failwith ""
+  (* Draws a card from stack, automatically executes action related to card.*)
+  | Draw_Chance -> failwith ""
+  | Draw_Community -> failwith ""
+
+  (* Buys a property.*)
+  | Buy s -> failwith ""
+  (* Sells a property for half the price bought.*)
+  | Sell s -> failwith ""
+
+  | Collect (s1, s2, s3) -> failwith ""
+  | _ -> failwith "Command should not have been run/DNE."
