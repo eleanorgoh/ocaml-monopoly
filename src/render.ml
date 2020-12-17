c6da358a4212fe461149762685709cfa1800b691
Eleanor Goh <eleanorgoh@Eleanors-MacBook-Pro.local>
2020-12-16 19:17:19 -0800

    Preparation for main sync

--- src/render.ml
+++ src/render.ml
@@ -34,33 +34,33 @@ let coords = [
   (100, 0)
 ]
 
-let prop_0 = init_property 1 "PSB" Light_Blue Property 0 1 2 3 4 5 10 20 0
-let prop_1 = init_property 0 "PSB" Green Property 0 1 2 3 4 5 1 1 1
-let prop_2 = init_property 0 "Home" Yellow Property 0 1 2 3 4 5 1 1 2
-let prop_3 = init_property 0 "Slope" Dark_Blue Property 0 1 2 3 4 5 1 1 3
-let prop_4 = init_property 0 "Line 92" Red Railroad 0 1 2 3 4 5 0 1 4
-let prop_5 = init_property 0 "Mac's" Red Property 0 1 2 3 4 5 1 9999 5
-let prop_6 = init_property 0 "Terrace" Pink Community_chest 1 1 1 1 1 1 1 9999 0
-let prop_7 = init_property 0 "Sage" Brown Go_to_jail 1 1 1 1 1 1 1 10 2
-let prop_8 = init_property 
+(* let prop_0 = init_property 1 "PSB" Light_Blue Property 0 1 2 3 4 5 10 20 0
+   let prop_1 = init_property 0 "PSB" Green Property 0 1 2 3 4 5 1 1 1
+   let prop_2 = init_property 0 "Home" Yellow Property 0 1 2 3 4 5 1 1 2
+   let prop_3 = init_property 0 "Slope" Dark_Blue Property 0 1 2 3 4 5 1 1 3
+   let prop_4 = init_property 0 "Line 92" Red Railroad 0 1 2 3 4 5 0 1 4
+   let prop_5 = init_property 0 "Mac's" Red Property 0 1 2 3 4 5 1 9999 5
+   let prop_6 = init_property 0 "Terrace" Pink Community_chest 1 1 1 1 1 1 1 9999 0
+   let prop_7 = init_property 0 "Sage" Brown Go_to_jail 1 1 1 1 1 1 1 10 2
+   let prop_8 = init_property 
     0 "Expensive Rent" Green Property 9999 9999 9999 9999 9999 9999 9999 10 0
-let prop_9 = init_property 0 "Olin" Pink Chance_card 1 1 1 1 1 1 1 10 4
-let prop_10 = init_property 0 "Libe" Orange Property 0 0 0 1 1 1 0 0 1
+   let prop_9 = init_property 0 "Olin" Pink Chance_card 1 1 1 1 1 1 1 10 4
+   let prop_10 = init_property 0 "Libe" Orange Property 0 0 0 1 1 1 0 0 1
 
-let go_prop = init_property 1 "GO" Light_Blue Go 0 1 2 3 4 5 10 20 0
-let tax_prop = init_property 1 "GO" Light_Blue Tax 0 1 2 3 4 5 10 20 0
-let in_jail_prop = 
-  init_property 1 "GO" Light_Blue In_jail_just_visiting 0 1 2 3 4 5 10 20 0
-let free_parking_prop = 
-  init_property 1 "GO" Light_Blue Free_parking 0 1 2 3 4 5 10 20 0
-let utility_prop = init_property 1 "GO" Light_Blue Utility 0 1 2 3 4 5 10 20 0
+   let go_prop = init_property 1 "GO" Light_Blue Go 0 1 2 3 4 5 10 20 0
+   let tax_prop = init_property 1 "GO" Light_Blue Tax 0 1 2 3 4 5 10 20 0
+   let in_jail_prop = 
+   init_property 1 "GO" Light_Blue In_jail_just_visiting 0 1 2 3 4 5 10 20 0
+   let free_parking_prop = 
+   init_property 1 "GO" Light_Blue Free_parking 0 1 2 3 4 5 10 20 0
+   let utility_prop = init_property 1 "GO" Light_Blue Utility 0 1 2 3 4 5 10 20 0
 
-let prop_lst_28 = [
-  go_prop; prop_0; prop_1; prop_2; prop_3; prop_4; prop_5; 
-  prop_6; utility_prop; prop_8; prop_9; prop_10; prop_0; prop_1; 
-  in_jail_prop; prop_3; prop_4; prop_5; tax_prop; prop_7; prop_8; 
-  prop_9; free_parking_prop; prop_4; prop_1; prop_2; prop_3; prop_5
-]
+   let prop_lst_28 = [
+   go_prop; prop_0; prop_1; prop_2; prop_3; prop_4; prop_5; 
+   prop_6; utility_prop; prop_8; prop_9; prop_10; prop_0; prop_1; 
+   in_jail_prop; prop_3; prop_4; prop_5; tax_prop; prop_7; prop_8; 
+   prop_9; free_parking_prop; prop_4; prop_1; prop_2; prop_3; prop_5
+   ] *)
 
 let convert_color = function 
   | Brown -> brown
@@ -125,8 +125,9 @@ let draw_tile_property coord prop =
   let color = prop |> Property.get_color |> convert_color in 
   set_color go_green; fill_rect (fst coord) (snd coord) sq_dim sq_dim;
   fill_header color coord;
-  draw_buildings (Property.get_num_buildings prop) (x + padding, 
-                                                    y + sq_dim - header_height + 2 * padding / 3) (building_color prop);
+  draw_buildings (Property.get_num_buildings prop) 
+    (x + padding, y + sq_dim - header_height + 2 * padding / 3) 
+    (building_color prop);
   let prop_name = Property.get_name prop in 
   let price = prop |> Property.get_price |> string_of_int in 
   draw_string_in_tile Center coord prop_name (sq_dim / 2) 15;
@@ -592,13 +593,13 @@ let wait =
     loop 0 0 
   with Graphic_failure _ -> print_endline "Exiting... Thanks for playing!"
 
-let () = init_window; 
-  try draw_board Main.sample_board; draw_monopoly_centerpiece 400 400; 
+(* let () = init_window; 
+   try draw_board Main.sample_board; draw_monopoly_centerpiece 400 400; 
     draw_camel_centerpiece;
     let rec loop x y  = 
       let _ = wait_next_event [Poll] and wx' = size_x () and wy' = size_y ()
       in loop wx' wy'
     in 
     loop 0 0 
-  with Graphic_failure _ -> print_endline "Exiting... Thanks for playing!"
+   with Graphic_failure _ -> print_endline "Exiting... Thanks for playing!" *)
 
