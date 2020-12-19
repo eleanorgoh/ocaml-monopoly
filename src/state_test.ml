38ffe559241a24e7df97b22e2fcb0a09633ee6bf
David Velasquez <dav83@cornell.edu>
2020-12-19 13:09:41 -0500

    finished state_test testing current_pos get_board in init

--- src/state_test.ml
+++ src/state_test.ml
@@ -4,83 +4,68 @@ open Newboard
 open Player
 open Test_utils
 
-type 
-  let get_player_pos_tests = [
-    one_arg_func_test "Players"  
+let board = Yojson.Basic.from_file "board.json"
+let game_board = from_json board
+
+let board_to_string = Yojson.Basic.to_string board
+
+let players_1 = [("David","Ship")]
+let players_2 = [("Alan", "Cat"); ("Sam", "Hat")]
+let players_3 = [("Alan", "Cat"); ("Sam", "Hat"); ("Eleanor", "Basket")]
+let state_1 = init_state game_board players_1
+let state_2 = init_state game_board players_2
+let state_3 = init_state game_board players_3
 
 
-
-  ]
-
-
-let board = json_board "board.json"
-let new_board = from_json board
-let state = [
-
+let current_pos_test = [
+  two_arg_func_test "Player: name = 'David', position = 0" 
+    current_pos state_1 "David" string_of_int 0;
+  two_arg_func_test "Player: name = 'Alan', position = 0" 
+    current_pos state_2 "Alan" string_of_int 0;
+  two_arg_func_test "Player: name = 'Sam', position = 0" 
+    current_pos state_2 "Sam" string_of_int 0;
+  two_arg_func_test "Player: name = 'Eleanor', position = 0" 
+    current_pos state_2 "Eleanor" string_of_int 0;
+  two_arg_func_test "Player: name = 'Sam', position = 0" 
+    current_pos state_2 "Sam" string_of_int 0;
 ]
-let player_1 = init_new_player "David" "Velasquez"
 
+let init_state_board_test
+    (name : string)
+    (state : State.t)
+    expected_output : test =
+  name >:: (fun _ -> 
+      assert_equal expected_output (get_board state))
 
-let init_state_tests = [
-  two_arg_getter "Player: name = 'Joe', marker = 'default' - test name" 
-    init_state "Joe" "default" get_name String.escaped "Joe";
-  two_arg_getter "Player: name = 'Bobo', marker = 'ugly' - test marker" 
-    init_state "Bobo" "ugly" get_marker_type String.escaped "ugly";
-  two_arg_getter "Player: name = 'Bobo', marker = 'ugly' - test money" 
-    init_state "Bobo" "ugly" get_money string_of_int 1500;
-  two_arg_getter "Player: name = 'Bobo', marker = 'ugly' - test properties" 
-    init_state "Bobo" "ugly" Player.get_properties 
-    (pp_list property_to_string) [];
+let init_state_board_tests = [
+  init_state_board_test "State: 1, Board: Board.json" state_1 game_board;
+  init_state_board_test "State: 2, Board: Board.json" state_2 game_board;
+  init_state_board_test "State: 3, Board: Board.json" state_3 game_board
 ]
-let card1 = init_card Community "Pay $200 for taxes" (Pay 200)
-let card2 = init_card Chance "Receive $200 from bank for birthday" (Receive 200)
-let card3 = init_card Community "Get out of jail free card" (OutJail)
-let card4 = init_card Chance "It's your unlucky day. Go to jail" (GoJail)
-let card5 = 
-  init_card Community "It's your lucky day. Advance to Go" (Advance "Go")
 
+let valid_command_test
+    (name : string)
+    (state : State.t)
+    (command : Command.t)
+    expected_output : test =
+  name >:: (fun _ -> 
+      assert_equal expected_output (valid_command state command))
 
-let card_tests = [
-  one_arg_func_test "Pay $200 for taxes, - test name"  
-    get_name card1 String.escaped "Pay $200 for taxes";
-  one_arg_func_test "Pay $200 for taxes, - test category"  
-    get_category card1 string_of_card Community;
-  one_arg_func_test "Pay $200 for taxes, - test action" 
-    get_action card1 string_of_action (Pay 200) ;
-
-  one_arg_func_test "Receive $200 from bank for birthday, - test name"  
-    get_name card2 String.escaped "Receive $200 from bank for birthday";
-  one_arg_func_test "Receive $200 from bank for birthday, - test category"  
-    get_category card2 string_of_card Chance;
-  one_arg_func_test "Receive $200 from bank for birthday, - test action" 
-    get_action card2 string_of_action (Receive 200);
-
-  one_arg_func_test "Get out of jail free card, - test name"  
-    get_name card3 String.escaped "Get out of jail free card";
-  one_arg_func_test "Get out of jail free card, - test category"  
-    get_category card3 string_of_card Community;
-  one_arg_func_test "Get out of jail free card, - test action" 
-    get_action card3 string_of_action OutJail;
-
-  one_arg_func_test "It's your unlucky day. Go to jail, - test name"  
-    get_name card4 String.escaped "It's your unlucky day. Go to jail";
-  one_arg_func_test "It's your unlucky day. Go to jail, - test category"  
-    get_category card4 string_of_card Chance;
-  one_arg_func_test "It's your unlucky day. Go to jail, - test action" 
-    get_action card4 string_of_action GoJail;
-
-  one_arg_func_test "It's your lucky day. Advance to Go, - test name"  
-    get_name card5 String.escaped "It's your lucky day. Advance to Go";
-  one_arg_func_test "It's your lucky day. Advance to Go, - test category"  
-    get_category card5 string_of_card Community;
-  one_arg_func_test "It's your lucky day. Advance to Go, - test action" 
-    get_action card5 string_of_action (Advance "Go");
-
+let valid_command_tests = [
+  valid_command_test "test for command = End_Turn" state_1 End_Turn true;
+  valid_command_test "test for command = Forfeit" state_1 Forfeit true;
+  valid_command_test "test for command = Roll" state_1 Roll true;
+  valid_command_test "test for command = Buy" state_1 (Buy "PSB") true;
+  valid_command_test "test for command = Sell" state_1 (Sell "PSB") false;
+  valid_command_test "test for command = Quit" state_1 Quit true;
 ]
+
 
 let suite =
   "test suite for card"  >::: List.flatten [
-    card_tests;
+    current_pos_test;
+    init_state_board_tests;
+    valid_command_tests
   ]
 
 let _ = run_test_tt_main suite
